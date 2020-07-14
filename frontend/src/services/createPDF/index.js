c27b8b411a6990991c37690662fd0986000da6ec
khammailawhang <khammaics@gmail.com>
2020-07-15 00:56:36 +0700

    Changes

--- frontend/src/services/createPDF/index.js
+++ frontend/src/services/createPDF/index.js
@@ -1,5 +1,5 @@
 import pdfMake from 'pdfmake/build/pdfmake';
-// import pdfFonts from './fonts/vfs_fonts';
+import pdfFonts from './fonts/vfs_fonts';
 // import console from 'console';
 
 import getTitle from './inspectionContents/titleContent';
@@ -13,52 +13,52 @@ import getFooter from './inspectionContents/footerContent';
 // import data from '../../../../backend/data/inspectiondpdf.json';
 // import data from './inspectionContents/carContent.js';
 
-// pdfMake.vfs = pdfFonts.pdfMake.vfs
+pdfMake.vfs = pdfFonts.pdfMake.vfs
 
-// pdfMake.fonts = {
-//     THSarabunNew: {
-//         normal: 'THSarabunNew.ttf',
-//         bold: 'THSarabunNew Bold.ttf',
-//         italics: 'THSarabunNew Italic.ttf',
-//         bolditalics: 'THSarabunNew BoldItalic.ttf'
-//     },
-//     LaoPhimPha: {
-//         normal: 'Lao_PhimPha_Regular.ttf',
-//         bold: 'Lao_PhimPha_Bold.ttf',
-//         italics: 'Lao_PhimPha_Regular.ttf',
-//         bolditalics: 'Lao_PhimPha_Regular.ttf'
-//     },
-//     LaoPhetsarath: {
-//         normal: 'Phetsarath OT.ttf',
-//         bold: 'Phetsarath OT.ttf',
-//         italics: 'Phetsarath OT.ttf',
-//         bolditalics: 'Phetsarath OT.ttf'
-//     },
-//     LaoThatPhon: {
-//         normal: 'That-Phon.ttf',
-//         bold: 'That-Phon.ttf',
-//         italics: 'That-Phon.ttf',
-//         bolditalics: 'That-Phon.ttf'
-//     },
-//     LaoDokkham: {
-//         normal: 'Lao_Dokkham.ttf',
-//         bold: 'Lao_Dokkham.ttf',
-//         italics: 'Lao_Dokkham.ttf',
-//         bolditalics: 'Lao_Dokkham.ttf'
-//     },
-//     LaoRajbandith: {
-//         normal: 'Lao_Rajbandith.ttf',
-//         bold: 'Lao_Rajbandith.ttf',
-//         italics: 'Lao_Rajbandith.ttf',
-//         bolditalics: 'Lao_Rajbandith.ttf'
-//     },
-//     Phetsarath: {
-//         normal: 'Phetsarath OT.ttf',
-//         bold: 'Phetsarath OT.ttf',
-//         italics: 'Phetsarath OT.ttf',
-//         bolditalics: 'Phetsarath OT.ttf'
-//     }
-// }
+pdfMake.fonts = {
+    THSarabunNew: {
+        normal: 'THSarabunNew.ttf',
+        bold: 'THSarabunNew Bold.ttf',
+        italics: 'THSarabunNew Italic.ttf',
+        bolditalics: 'THSarabunNew BoldItalic.ttf'
+    },
+    LaoPhimPha: {
+        normal: 'Lao_PhimPha_Regular.ttf',
+        bold: 'Lao_PhimPha_Bold.ttf',
+        italics: 'Lao_PhimPha_Regular.ttf',
+        bolditalics: 'Lao_PhimPha_Regular.ttf'
+    },
+    LaoPhetsarath: {
+        normal: 'Phetsarath OT.ttf',
+        bold: 'Phetsarath OT.ttf',
+        italics: 'Phetsarath OT.ttf',
+        bolditalics: 'Phetsarath OT.ttf'
+    },
+    LaoThatPhon: {
+        normal: 'That-Phon.ttf',
+        bold: 'That-Phon.ttf',
+        italics: 'That-Phon.ttf',
+        bolditalics: 'That-Phon.ttf'
+    },
+    LaoDokkham: {
+        normal: 'Lao_Dokkham.ttf',
+        bold: 'Lao_Dokkham.ttf',
+        italics: 'Lao_Dokkham.ttf',
+        bolditalics: 'Lao_Dokkham.ttf'
+    },
+    LaoRajbandith: {
+        normal: 'Lao_Rajbandith.ttf',
+        bold: 'Lao_Rajbandith.ttf',
+        italics: 'Lao_Rajbandith.ttf',
+        bolditalics: 'Lao_Rajbandith.ttf'
+    },
+    Phetsarath: {
+        normal: 'Phetsarath OT.ttf',
+        bold: 'Phetsarath OT.ttf',
+        italics: 'Phetsarath OT.ttf',
+        bolditalics: 'Phetsarath OT.ttf'
+    }
+}
 
 // Upload to Spaces in DigitalOcean
 async function uploadToSpaces(blob) {
